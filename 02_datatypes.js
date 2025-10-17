d5f08dab499f0b491c9407c8628555003153c1d6
Ayush Sharma <as3848274@gmail.com>
2025-10-17 17:23:33 +0530

    Add 02_datatypes.js with various data type examples

---
+++ 02_datatypes.js
@@ -0,0 +1,66 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(valueInNumber); // gives NaN
+
+// "33"=> 33
+//"33abc" => NaN
+// "true" => 1
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//1 gets converted to true
+// ""=> false
+//"Ayush"=> true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// it get converted to string
+// 33 => "33"
+
+
+// OPERATIONS 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hitesh"
+// let str2  = "hello "
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+3); // 13
+// console.log(1+"2");// 12
+// console.log("1"+"2");// 12
+// console.log("1"+ 2 + 2 );//122
+// console.log(2 + 2 + "1" );//41
+
+// cosole.log(+true); // possible and gives output 1
+// console.log(true+);// not possible and gives error
+// console.log(+"") // 0
+
+
+// let num1 , num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++; // by using this it will give us 101 
+// ++gameCounter  // gives the same value as 101
+// console.log(gameCounter);
+
+
+
+
+
+
+
+
