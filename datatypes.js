c0dbae079472fb5f493017239d9cdbaf812e1e90
Ayush Sharma <as3848274@gmail.com>
2025-10-17 17:21:38 +0530

    Add datatypes.js with basic JavaScript examples

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict " ; //treat all JS code as newer version
+// alert (3+3) we are using node js not browser 
+console.log (3+3)
+console.log("AYUSH")
+//code readability must be high 
+let name = "Ayush"
+let age = 18 
+let isLoggedIn = false 
+let state ;
+
+// number = 2 to the power of 53
+//bigint 
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbol => unique
+
+
+//object 
+console.log(typeof "hitesh");
+console.log (typeof null); //object
+console.log( typeof undefined) ; // undefined 
+
+
