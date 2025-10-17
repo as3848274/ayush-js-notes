35655a8051165694e78da14666722a8648a11816
Ayush Sharma <as3848274@gmail.com>
2025-10-17 17:27:34 +0530

    Implement string manipulation examples in 05_strings.js

---
+++ 05_strings.js
@@ -0,0 +1,36 @@
+// const name = "hitesh"
+// const repoCount = 50
+//console.log(name + repoCount + "value");
+// console.log(' Hello my name is ${name} and my repo count is ${repoCount} ');
+
+
+ const gameName  = new String ('ayush-sharma')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.chatAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-3,2)
+// console.log(anotherString);
+
+// const newStringOne = "    ayush   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://ayush.com/aboutname%20ayushsharma"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes ('ayush'));
+
+console.log(gameName.split('-'))
+
+
+
+
