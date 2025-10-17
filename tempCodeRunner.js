7e9cad1c3627e318a2f9dc897a60c602d1a8d537
Ayush Sharma <as3848274@gmail.com>
2025-10-17 17:22:22 +0530

    Add initial date manipulation code in tempCodeRunner.js

---
+++ tempCodeRunner.js
@@ -0,0 +1,8 @@
+let myDate = new Date();
+// console.log(myDate.toString()); // Current date and time
+// console.log(myDate.toDateString());  
+// console.log(myDate.toLocalString()); 
+// console.log(typeof myDate); // output: object
+
+// let myCreatedDate = new Date(2020, 0, 31); // January is 0
+// console.log(myCreatedDate); 
