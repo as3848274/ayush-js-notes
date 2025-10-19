25f7bb9798d923532b4150859ddd5a9f12b5ea33
Ayush Sharma <as3848274@gmail.com>
2025-10-19 10:49:10 +0530

    Create 23_loops.js

---
+++ 23_loops.js
@@ -0,0 +1,42 @@
+// const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+//const values = coding.forEach ( (item) => {
+  // console.log(item);
+  // return item
+// })
+// console.log(values);
+
+// for Each method do not return anything at any cost always show undefined 
+
+
+// BASICS OF FILTER 
+ const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);
+
+// learn another syntax 
+// qki yha condition se phle curly braces lgaa diye ya curly braces se start hua to ab return lgaane ki need hai qki yha scope open hogyA
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => {
+//   return num > 4
+// })
+// console.log(newNums);
+
+
+
+// const newNums = []
+
+//  myNums.forEach( (num) => {
+//   if (num > 4) {
+//      newNums.push(num)
+//   }
+// })
+// console.log(newNums);
+
+
+
+
+
