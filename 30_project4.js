1960b3de961b1fd9dc038640bf75499e12a0ba6c
Ayush Sharma <as3848274@gmail.com>
2025-10-26 13:08:12 +0530

    Create 30_project4.js

---
+++ 30_project4.js
@@ -0,0 +1,15 @@
+// digital clock project
+
+
+const clock = document.getElementById('clock')
+//const clock = const clock = document.querySelector('#clock')
+
+
+
+setInterval(function(){
+  let date = new Date()
+  // console.log(date.toLocaleString());
+  clock.innerHTML = date.toLocaleString();
+
+} , 1000);
+
